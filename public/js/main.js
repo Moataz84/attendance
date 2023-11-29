d0a858b58adfac6d730fd5e78212b258be08b87b
Moataz Ghazy <m.ghazy44@hotmail.com>
2023-11-29 11:28:30 -0500

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,12 @@ function sendStudent(e) {
   e.preventDefault()
   const username = window.location.href.split("/").splice(-1)[0]
   socket.emit("send-student", username)
+  window.location.href = "/"
 }
 
 function signOutStudent(e) {
   e.preventDefault()
   const username = window.location.href.split("/").splice(-1)[0]
   socket.emit("sign-out-student", username)
+  window.location.href = "/"
 }
